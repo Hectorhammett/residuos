07be0f1c1346dafe3dee761e2793f2839f86bedd
Hector Mendoza <Hector Mendoza>
2016-02-08 02:17:10 -0800

    -Changed from Electron to NWJS

---
+++ js/start.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  getPage("index-page.html");
+})
+
+//handlers ----------------------------------------------------------------------------
+
+$("#main-menu li a").click(function(e){
+  var link = this;
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  $('.active-menu').removeClass('active-menu');
+  $(link).addClass('active-menu');
+  getPage($(link).prop('href'));
+})
+
+//functions----------------------------------------------------------------------------
+function getPage(url){
+  $.get(url,function(response){
+    pastePage(response);
+  })
+  .fail(function(xhr,textStatus,errorThrown){
+    alert("Hubo un error en la aplicación: " + textStatus);
+  });
+}
+
+function pastePage(data){
+  $('#page-inner').html(data);
+}
